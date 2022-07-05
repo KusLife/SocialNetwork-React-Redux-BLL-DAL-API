5baed2b222542f615098666220fd87dc1400c47a
KusLIfe <larisovkirill@gmail.com>
2022-07-05 19:59:41 +0300

    Updates

---
+++ static/js/770.472cfd97.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[770],{6770:function(e,n,t){t.r(n),t.d(n,{default:function(){return c}});var s=t(7581),i=t(5927),r=(t(2791),t(3504)),a=t(7627),u=t(184),c=(0,s.$j)((function(e){return{eventsList:e.eventsData.eventsList}}))((0,i.Z)((function(e){var n=e.eventsList.map((function(e){return(0,u.jsx)("div",{children:(0,u.jsx)(r.rU,{to:"events/"+e.id,children:e.data})})}));return(0,u.jsx)("div",{className:a.Z.item,children:n})})))}}]);
+//# sourceMappingURL=770.472cfd97.chunk.js.map
